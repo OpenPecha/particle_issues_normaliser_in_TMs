a9597f6cad309a3d50a6edd633ba41ac6ae0cdb1
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-15 02:09:03 +0530

    FIX_spaces_difference_from_antxAnnotationTransfer_function

--- src/antxAnnotationTransfer.py
+++ src/antxAnnotationTransfer.py
@@ -17,7 +17,7 @@ def antxAnnotationTransfer(source_text, target_text):
 
 def remove_newlines(text):
     # Use regex to remove newlines
-    text_without_newlines = re.sub(r"\n", "", text)
+    text_without_newlines = re.sub(r"\n", " ", text)
 
     return text_without_newlines
 
@@ -27,8 +27,6 @@ if __name__ == "__main__":
     target_text = Path("./BO0790_segmented.txt").read_text(encoding="utf-8")
     target_text = remove_newlines(target_text)
     AnnotatedText = antxAnnotationTransfer(source_text, target_text)
-    counter = 0
     with open("./new_TM0790.txt", "w", encoding="utf-8") as file:
-        file.write(AnnotatedText)
-
-    print(counter)
+        for AnnotatedLine in AnnotatedText.splitlines():
+            file.write(f"{AnnotatedLine.strip()}\n")
