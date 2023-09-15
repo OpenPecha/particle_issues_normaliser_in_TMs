05486cfee94a7db28c690145a9dc3d2a57fe9894
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-15 11:09:19 +0530

    Modify_function_repo_file_uploader

--- src/repo_file_uploader.py
+++ src/repo_file_uploader.py
@@ -1,6 +1,6 @@
-import base64
+from pathlib import Path
 
-import requests
+from github import Github
 
 from config import github_token_access
 
@@ -12,37 +12,35 @@ class GitHubFileUploader:
         self.token = token
 
     def get_file_sha(self, file_path, branch="main"):
-        url = f"https://api.github.com/repos/{self.repo_owner}/{self.repo_name}/contents/{file_path}?ref={branch}"
-        response = requests.get(url, headers={"Authorization": f"token {self.token}"})
-        if response.status_code == 200:
-            return response.json()["sha"]
-        else:
-            raise Exception(f"Error: {response.status_code} - {response.json()}")
+        g = Github(self.token)
+        # Get the repository
+        repo = g.get_repo(f"{self.repo_owner}/{self.repo_name}")
+        try:
+            # Try to get the existing file (if it exists)
+            existing_file = repo.get_contents(file_path, ref=branch)
+            return existing_file.sha
+        except Exception:
+            return None  # File doesn't exist, so current SHA is None
 
-    def upload_txt_file(self, file_path, branch="main"):
-        sha = self.get_file_sha(file_path, branch)
-        url = f"https://api.github.com/repos/{self.repo_owner}/{self.repo_name}/contents/{file_path}"
-        content = open(file_path).read()
-        encoded_content = base64.b64encode(
-            content.encode()
-        ).decode()  # Encode content to base64
-        response = requests.put(
-            url,
-            json={
-                "message": "Update file via script",
-                "content": encoded_content,
-                "sha": sha,
-                "branch": branch,
-            },
-            headers={
-                "Authorization": f"token {self.token}",
-                "Accept": "application/vnd.github.v3+json",
-            },
-        )
-        if response.status_code == 200:
-            print(f'File "{file_path}" updated successfully!')
+    def upload_txt_file(self, file_path, file_data):
+        g = Github(self.token)
+        # Get the repository
+        repo = g.get_repo(f"{self.repo_owner}/{self.repo_name}")
+
+        # Get the current SHA hash of the file
+        current_sha = self.get_file_sha(file_path)
+
+        # Specify the commit message
+        commit_message = "Update file via script"
+
+        if current_sha:
+            # File exists, update it
+            repo.update_file(
+                file_path, commit_message, file_data, current_sha, branch="main"
+            )
         else:
-            print(f"Error: {response.status_code} - {response.json()}")
+            # File doesn't exist, create it
+            repo.create_file(file_path, commit_message, file_data, branch="main")
 
 
 if __name__ == "__main__":
@@ -52,4 +50,6 @@ if __name__ == "__main__":
     repo_name = "test_repo"
 
     uploader = GitHubFileUploader(token, repo_owner, repo_name)
-    uploader.upload_txt_file("new_TM0790.txt")
+    file_path = "TM0790-bo.txt"
+    file_data = Path("TM0790_cleaned.txt").read_text(encoding="utf-8")
+    uploader.upload_txt_file(file_path, file_data)
