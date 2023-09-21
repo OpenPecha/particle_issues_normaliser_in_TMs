cc39f6468179ebd18a3a6b28722f10e8aef9721f
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-21 15:23:13 +0530

    Update_add_progress_message_to_function_sentence_tokenize_and_save_in_folder

--- src/tibetan_sentence_tokenizer_pipeline.py
+++ src/tibetan_sentence_tokenizer_pipeline.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 from op_mt_tools.tokenizers import bo_sent_tokenizer as sentence_tokenizer
 
+from affix_check_script import count_files_in_folder
+from config import FILTERED_TM_FOLDER_DIR  # noqa
+from config import FILTERED_TOKENIZED_TM_FOLDER_DIR  # noqa
 from config import FILTERED_BO_FOLDER_DIR, FILTERED_TOKENIZED_BO_FOLDER_DIR
 
 
@@ -12,21 +15,18 @@ def sentence_tokenizer_pipeline(text: str) -> str:
 
 def sentence_tokenize_and_save_in_folder(folder_path: Path, output_folder_path: Path):
     txt_files = folder_path.glob("*.txt")
-    counter = 0
+    file_count = count_files_in_folder(folder_path)
+    counter = 1
     for txt_file in txt_files:
+        print(f"[{counter}/{file_count}]] File {txt_file.name}...")
         file_content = txt_file.read_text(encoding="utf-8")
         tokenized_content = sentence_tokenizer_pipeline(file_content)
         output_file_path = Path(output_folder_path) / txt_file.name
         output_file_path.write_text(tokenized_content)
-        if counter == 3:
-            break
         counter += 1
 
 
 if __name__ == "__main__":
-    # bo_string = "༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ འི་ ཕྲེང་བ། ལ་ ལ་ལ་ ལ་ ལ་བ་ ཡོད། དཔལ། དགེའོ་ བཀྲ་ཤིས་ ཤོག།"
-    # bo_string_tokenized = sentence_tokenizer_pipeline(bo_string)
-    # print(bo_string_tokenized)
     sentence_tokenize_and_save_in_folder(
         FILTERED_BO_FOLDER_DIR, FILTERED_TOKENIZED_BO_FOLDER_DIR
     )
