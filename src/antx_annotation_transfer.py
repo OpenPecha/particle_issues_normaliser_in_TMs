f6eea80a338d694ad2bc44193b11e2f11048ae61
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-22 15:05:49 +0530

    FEAT_function_annotation_transfer_and_save_in_folder

--- src/antx_annotation_transfer.py
+++ src/antx_annotation_transfer.py
@@ -1,7 +1,9 @@
 import re
+from pathlib import Path
 
 from antx import transfer
 
+from affix_check_script import count_files_in_folder
 from config import (
     FILTERED_TM_FOLDER_DIR,
     FILTERED_TOKENIZED_CLEANED_TM_FOLDER_DIR,
@@ -18,25 +20,47 @@ def antx_annotation_transfer(source_text, target_text):
         ["new_line", r"(\n)"],
         ["space_line", r"(\s+)"],
     ]
+    target_text = remove_newlines_tabs_spaces(target_text)
     annotated_text = transfer(source_text, annotations, target_text, output="txt")
     return annotated_text
 
 
+def annotation_transfer_and_save_in_folder(
+    source_folder_path: Path, target_folder_path, output_folder_path
+):
+    txt_files = source_folder_path.glob("*.txt")
+    file_count = count_files_in_folder(source_folder_path)
+    counter = 1
+
+    for txt_file in txt_files:
+        print(f"[{counter}/{file_count}]] File {txt_file.name}...")
+        source_content = txt_file.read_text(encoding="utf-8")
+        target_file_path = target_folder_path / txt_file.name
+        target_content = target_file_path.read_text(encoding="utf-8")
+        annotated_content = antx_annotation_transfer(source_content, target_content)
+        output_file_path = Path(output_folder_path) / txt_file.name
+        output_file_path.write_text(annotated_content, encoding="utf-8")
+        counter += 1
+
+
 def remove_newlines_tabs_spaces(text):
     # Use regex to remove newlines
-    text = re.sub(r"\n", "", text)
-    text = re.sub(r"\t", "", text)
-    text = re.sub(r"\s+", "", text)
+    text = re.sub(r"[\n\t\s]+", "", text)
     return text
 
 
 if __name__ == "__main__":
-    file_name = "TM0001.txt"
-    source_text = (FILTERED_TM_FOLDER_DIR / file_name).read_text(encoding="utf-8")
-    target_text = (FILTERED_TOKENIZED_CLEANED_TM_FOLDER_DIR / file_name).read_text(
-        encoding="utf-8"
+    annotation_transfer_and_save_in_folder(
+        FILTERED_TM_FOLDER_DIR,
+        FILTERED_TOKENIZED_CLEANED_TM_FOLDER_DIR,
+        FINAL_CLEANED_ANNOTATED_TM_FOLDER_DIR,
     )
-    target_text = remove_newlines_tabs_spaces(target_text)
-    annotated_text = antx_annotation_transfer(source_text, target_text)
-    output_file_path = FINAL_CLEANED_ANNOTATED_TM_FOLDER_DIR / file_name
-    output_file_path.write_text(annotated_text, encoding="utf-8")
+    # file_name = "TM0791.txt"
+    # source_text = (FILTERED_TM_FOLDER_DIR / file_name).read_text(encoding="utf-8")
+    # target_text = (FILTERED_TOKENIZED_CLEANED_TM_FOLDER_DIR / file_name).read_text(
+    #     encoding="utf-8"
+    # )
+    # target_text = remove_newlines_tabs_spaces(target_text)
+    # annotated_text = antx_annotation_transfer(source_text, target_text)
+    # output_file_path = FINAL_CLEANED_ANNOTATED_TM_FOLDER_DIR / file_name
+    # output_file_path.write_text(annotated_text, encoding="utf-8")
