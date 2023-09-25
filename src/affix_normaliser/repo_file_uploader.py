9aab3fa6c3112ae581ffc31a676f3e3b2403c96b
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-25 18:07:20 +0530

    FEAT_function_upload_files_in_folder_to_repo

--- src/affix_normaliser/repo_file_uploader.py
+++ src/affix_normaliser/repo_file_uploader.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 import requests
 from github import Github
 from retrying import retry
 
+from .affix_check_script import count_files_in_folder
 from .config import TM_FOLDER_DIR
 from .settings import GITHUB_TOKEN
+
+TOKEN = GITHUB_TOKEN
+REPO_OWNER = "tenzin3"
 
 
 class GitHubFileUploader:
@@ -14,10 +20,8 @@ class GitHubFileUploader:
 
     def get_file_sha(self, file_path, branch="main"):
         g = Github(self.token)
-        # Get the repository
         repo = g.get_repo(f"{self.repo_owner}/{self.repo_name}")
         try:
-            # Try to get the existing file (if it exists)
             existing_file = repo.get_contents(file_path, ref=branch)
             return existing_file.sha
         except Exception:
@@ -32,7 +36,6 @@ class GitHubFileUploader:
     )
     def upload_txt_file(self, file_path, file_data):
         g = Github(self.token)
-        # Get the repository
         repo = g.get_repo(f"{self.repo_owner}/{self.repo_name}")
 
         # Get the current SHA hash of the file
@@ -49,6 +52,22 @@ class GitHubFileUploader:
         else:
             # File doesn't exist, create it
             repo.create_file(file_path, commit_message, file_data, branch="main")
+
+
+def upload_files_in_folder_to_repo(folder_path: Path):
+    txt_files = folder_path.glob("*.txt")
+    file_count = count_files_in_folder(folder_path)
+    counter = 1
+
+    for txt_file in txt_files:
+        print(f"[{counter}/{file_count}]] File {txt_file.name} Uploading...")
+        file_content = txt_file.read_text(encoding="utf-8")
+
+        file_name = txt_file.name
+        repo_name = txt_file.name
+        uploader = GitHubFileUploader(token, repo_owner, repo_name)
+        uploader.upload_txt_file(file_name, file_content)
+        counter += 1
 
 
 if __name__ == "__main__":
